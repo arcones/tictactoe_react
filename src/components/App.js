16aa28baeceec78370cb4272aca32d52b5efda44
sonsoleslp <poynting.slp@gmail.com>
2020-11-11 19:01:21 +0100

    step6

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header.jsx';
 import Board from './Board.jsx';
+import Reset from './Reset.jsx';
 
 const PLAYERX = "Player 1 - Xs";
 const PLAYER0 = "Player 2 - 0s";
@@ -10,6 +11,7 @@ export default class App extends React.Component {
     super(props);
     this.state = {
         turn: PLAYERX,
+        moves: 0,
         values: [
         ['-', '-', '-'],
         ['-', '-', '-'],
@@ -17,17 +19,32 @@ export default class App extends React.Component {
         ],
     };
     this.appClick = this.appClick.bind(this);
-    }
+    this.resetClick = this.resetClick.bind(this);
+  }
 
-    appClick(rowNumber, columnNumber) {
-        let valuesCopy = JSON.parse(JSON.stringify(this.state.values));
-        let newMovement = this.state.turn === PLAYERX ? 'X' : '0';
-        valuesCopy[rowNumber][columnNumber] = newMovement;
-        this.setState({
-            turn: this.state.turn === PLAYERX ? PLAYER0 : PLAYERX,
-            values: valuesCopy,
-        });
-    }
+  appClick(rowNumber, columnNumber) {
+      let valuesCopy = JSON.parse(JSON.stringify(this.state.values));
+      let newMovement = this.state.turn === PLAYERX ? 'X' : '0';
+      valuesCopy[rowNumber][columnNumber] = newMovement;
+      this.setState({
+          turn: this.state.turn === PLAYERX ? PLAYER0 : PLAYERX,
+          values: valuesCopy,
+          moves: this.state.moves + 1
+      });
+  }
+
+  resetClick(){
+    this.setState({
+      turn: PLAYERX,
+      values: [
+        ['-', '-', '-'],
+        ['-', '-', '-'],
+        ['-', '-', '-'],
+      ],
+      winner: undefined,
+      moves: 0
+    });
+  }
 
   render() {
     let text = "Turn of " + this.state.turn;
@@ -36,6 +53,8 @@ export default class App extends React.Component {
       <div>
         <Header text={text}/>
         <Board values={this.state.values}  appClick={this.appClick}/>
+        <h3>Number of moves: {this.state.moves}</h3>
+        <Reset resetClick={this.resetClick}></Reset>
       </div>
     );
 }
