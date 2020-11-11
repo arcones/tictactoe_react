69d3229de54d46f9080a0ec6a160d562dbaa8d25
sonsoleslp <poynting.slp@gmail.com>
2020-11-11 18:47:01 +0100

    step5

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,25 +2,32 @@ import React from 'react';
 import Square from './Square';
 
 export default class Board extends React.Component {
-    render() {
-        let board = this.props.values.map((rowValues, rowIndex) => {
-            let row = rowValues.map((value, columnIndex) => {
-                let mykey = "" + rowIndex + columnIndex;
-                return (
-                  <Square value={value} key={mykey}/>
-                );
-            });
-            return (
-               <div key={"row" + rowIndex}>
-                 {row}
-               </div>
-            );
-        });
-
-        return (
-           <div>
-             {board}
-           </div>
+  constructor(props) {
+    super(props);
+    this.boardClick = this.boardClick.bind(this);
+  }
+  boardClick(rowNumber, columnNumber) {
+    this.props.appClick(rowNumber, columnNumber);
+  }
+  render() {
+    let board = this.props.values.map((rowValues, rowIndex) => {
+      let row = rowValues.map((value, columnIndex) => {
+          return (
+            <Square value={value} key={rowIndex + "-" + columnIndex} rowIndex={rowIndex}
+                    columnIndex={columnIndex} boardClick={this.boardClick}/>
         );
-    }
+      });
+      return (
+        <div key={"row" + rowIndex}>
+          {row}
+        </div>
+      );
+    });
+
+    return (
+       <div>
+        {board}
+       </div>
+    );
+  }
 }
