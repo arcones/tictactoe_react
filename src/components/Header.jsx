e0406ededfca545cba0f8ce7b0bfe9a415e29799
sonsoleslp <poynting.slp@gmail.com>
2020-11-11 19:11:32 +0100

    step7

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import {Card} from 'react-bootstrap';
 
 export default class Header extends React.Component {
     render() {
         return (
-          <header className="header">
-            {this.props.text}
-          </header>
+          <Card style={{ width: '18rem' }}>
+            <Card.Body>
+              <Card.Title>Turn</Card.Title>
+              <Card.Text>
+               {this.props.text}
+               
+              </Card.Text>
+            </Card.Body>
+          </Card>
         );
     }
 
 }
+
--- src/components/Reset.jsx
+++ src/components/Reset.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Button} from 'react-bootstrap';
 
 export default class Reset extends React.Component {
     constructor(props) {
@@ -12,9 +13,9 @@ export default class Reset extends React.Component {
 
     render() {
         return(
-          <button onClick={this.click} >
+          <Button onClick={this.click} >
             Reset
-          </button>
+          </Button>
         );
     }
 }
